e416e905a81f2c06b0efb92f084a12c760ecbc09
StarAssassin64 <michaelrprobasco@gmail.com>
2022-05-02 21:23:24 -0500

    adding commands (not fully working)
    Signed-off-by: StarAssassin64 <michaelrprobasco@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Modules
-const {Client, Intents} = require("discord.js");
+const {Client, Intents, Interaction} = require("discord.js");
 const { token } = require("./setup.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
@@ -8,4 +8,18 @@ client.once("ready", () => {
     console.log("Ready");
 });
 
-client.login(token)
+client.on('interactionCreate', async Interaction => {
+    if(!Interaction.isCommand()) return;
+
+    const { commandName } = Interaction;
+
+    if (commandName === "ping") {
+        await Interaction.reply("Pong!");
+    } else if (commandName === "server") {
+        await Interaction.reply("Server Info.");
+    } else if (commandName === "user") {
+        await Interaction.reply("User Info.");
+    };
+});
+
+client.login(token);
