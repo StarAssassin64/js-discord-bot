8bc672a3f97e15242b925ac7484e3779300ad219
StarAssassin64 <64805947+StarAssassin64@users.noreply.github.com>
2022-05-17 17:02:18 -0500

    updated things, going to delete other branch

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 //Modules
-const {Client, Intents, Interaction} = require("discord.js");
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Intents, Interaction, Collection} = require("discord.js");
 const { token } = require("./setup.json");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	
+	client.commands.set(command.data.name, command);
+}
 
 client.once("ready", () => {
     console.log("Ready");
 });
 
 client.on('interactionCreate', async Interaction => {
-    if(!Interaction.isCommand()) return;
-
-    const { commandName } = Interaction;
-
-    if (commandName === "ping") {
-        await Interaction.reply("Pong!");
-    } else if (commandName === "server") {
-        await Interaction.reply("Server Info.");
-    } else if (commandName === "user") {
-        await Interaction.reply("User Info.");
-    };
+	if(!Interaction.isCommand) return;
+	
+	const command = client.commands.get(Interaction.commandName);
+	
+	if (!command) return;
+	try {
+		await command.execute(Interaction);
+	} catch (error) {
+		console.error(error);
+		await Interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+	}
 });
 
 client.login(token);
